89b918f323b613c1afb2f1ef396ddcdab3299bff
nishchayjeet01 <90867938+nishchayjeet01@users.noreply.github.com>
2022-07-24 06:18:16 +0530

    UP

---
+++ junk/nto1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void func(int i, intn){
+    if(i<n) {
+        return;
+    }
+    cout<<n<<endl;
+    func(i,n-1);
+}
+
+int main(){
+    int i,n;
+    func(1,4);
+}
